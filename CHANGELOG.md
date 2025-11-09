d19b436fcb9a40a0e66830bbb7b071698297519c
miraprobemv <miraprobemv@gmail.com>
2025-11-09 21:35:58 +0900

    ビルド情報の更新

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## [0.1.0]
 
 - Added multiple preview panel mode
+- Added the ability to select a glyph from the glyph table.
 - Added view options
   - Show curvature combs
   - Show preview image
--- webpack.config.js
+++ webpack.config.js
@@ -110,11 +110,6 @@ const webviewConfig = {
 			}],
 		}],
 	},
-	plugins: [
-		new webpack.DefinePlugin({
-		"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-		}),
-	],
 };
 
 module.exports = [webExtensionConfig, webviewConfig];
