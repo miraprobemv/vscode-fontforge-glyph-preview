73de5adb922d5ccfb613dfc40be6753f8b76f350
miraprobemv <miraprobemv@gmail.com>
2025-11-24 09:35:46 +0900

    ビルド設定を更新

--- webpack.config.js
+++ webpack.config.js
@@ -111,10 +111,10 @@ const webviewConfig = {
 		}],
 	},
 	plugins: [
-		...(process.env.NODE_ENV ? [
+		...(!process.argv.includes("production") ? [
 			new webpack.DefinePlugin({
-				"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-			})
+				"process.env.NODE_ENV": process.env.NODE_ENV,
+			}),
 		] : [
 		]),
 	],
